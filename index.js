abecb6382ac41615ab45d0d45a836eb802cbd5b0
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-05 22:20:45 +0300

    modif.

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ app.use(cors());
 dotenv.config({ path: '.env' })
 
 
-
-
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
