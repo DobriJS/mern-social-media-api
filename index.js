015679eae9ecad3f7c57e88c6b7a76e331f56953
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-05 22:50:28 +0300

    Remove comments

--- index.js
+++ index.js
@@ -3,16 +3,12 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+dotenv.config({ path: '.env' })
 const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
-dotenv.config({ path: '.env' })
 
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
-
-
-
-/* mongodb://DobriJS:<password>@cluster0-shard-00-00.myt24.mongodb.net:27017,cluster0-shard-00-01.myt24.mongodb.net:27017,cluster0-shard-00-02.myt24.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-v5slnn-shard-0&authSource=admin&retryWrites=true&w=majority */
