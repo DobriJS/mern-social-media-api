11e88f2e79549198dc1d6395af25f024419bf339
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-05 23:25:50 +0300

    Create routes for posts

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
 
 dotenv.config({ path: '.env' })
 const app = express();
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
-
+app.use('/posts', postRoutes);
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
