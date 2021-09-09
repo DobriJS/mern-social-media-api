7884fb7c69fc6ad39da195a39644743478b81016
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-09 13:31:49 +0300

    Connection URL

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(process.env.PORT, () => console.log(`Server Running on Port: http://localhost:${process.env.PORT}`)))
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
