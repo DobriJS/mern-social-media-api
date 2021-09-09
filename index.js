35a18594d2ddfeff6db485277e8d954aba03df1a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-09 13:43:13 +0300

    Port

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb://DobriJS:tGwe7DhTR11xoIpA@cluster0-shard-00-00.myt24.mongodb.net:27017,cluster0-shard-00-01.myt24.mongodb.net:27017,cluster0-shard-00-02.myt24.mongodb.net:27017/mern-social?ssl=true&replicaSet=atlas-v5slnn-shard-0&authSource=admin&retryWrites=true&w=majority'
+const PORT = process.env.PORT|| 4000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
