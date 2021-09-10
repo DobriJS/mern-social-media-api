a392049fd391fcf7ed14ebd4f7158ff81a348407
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-10 15:23:58 +0300

    Add jsonwebtoken and brypt

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.use('/posts', postRoutes);
 const CONNECTION_URL = 'mongodb://DobriJS:tGwe7DhTR11xoIpA@cluster0-shard-00-00.myt24.mongodb.net:27017,cluster0-shard-00-01.myt24.mongodb.net:27017,cluster0-shard-00-02.myt24.mongodb.net:27017/mern-social?ssl=true&replicaSet=atlas-v5slnn-shard-0&authSource=admin&retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 4000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
