a61955ee01d4c8a4d2fcca9513377b9b01bf2d77
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-21 22:14:39 +0300

    test

--- controllers/posts.js
+++ controllers/posts.js
@@ -68,52 +68,68 @@ export const createPost = async (req, res) => {
 }
 
 export const updatePost = async (req, res) => {
-    const { id } = req.params;
-    const { title, message, creator, selectedFile, tags } = req.body;
+    const { id: _id } = req.params;
+	const { title, message, selectedFile, creator, tags } = req.body;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send(`No post with id: ${id}`);
-    }
+	if (!mongoose.Types.ObjectId.isValid(_id))
+		return res.status(404).send("No post with that id");
 
-    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
-    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+	try {
+		const updatePost = await PostMessage.findByIdAndUpdate(
+			_id,
+			{
+				title,
+				message,
+				selectedFile: selectedFile,
+				creator,
+				tags,
+				_id,
+			},
+			{
+				new: true,
+			}
+		);
 
-    res.json(updatedPost);
+		return res.status(200).json(updatePost);
+	} catch (error) {
+		return res.status(409).json({ message: error });
+	}
 }
 
 export const deletePost = async (req, res) => {
-    const { id } = req.params;
+    const { id: _id } = req.params;
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send(`No post with id: ${id}`);
-    }
+	if (!mongoose.Types.ObjectId.isValid(_id))
+		return res.status(404).send("No post with that id");
 
-    await PostMessage.findByIdAndRemove(id);
+	await PostMessage.findByIdAndRemove(_id);
 
-    res.json({ message: "Post deleted successfully." });
+	res.json({ message: "Post deleted successfully" });
 }
 
 export const likePost = async (req, res) => {
-    const { id } = req.params;
+    const { id: _id } = req.params;
 
-    if (!req.userId) {
-        return res.json({ message: "Unauthenticated" });
-      }
+	if (!req.userId) return res.json({ message: "Unauthenticated" });
 
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).send(`No post with id: ${id}`);
-    }
+	if (!mongoose.Types.ObjectId.isValid(_id))
+		return res.status(404).send("No post with that id");
 
-    const post = await PostMessage.findById(id);
-    const index = post.likes.findIndex((id) => id === String(req.userId));
+	const post = await PostMessage.findById(_id);
 
-    if (index === -1) {
-      post.likes.push(req.userId);
-    } else {
-      post.likes = post.likes.filter((id) => id !== String(req.userId));
-    }
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
-    res.status(200).json(updatedPost);
+	const index = post.likes.findIndex((id) => id === String(req.userId));
+
+	if (index === -1) {
+		post.likes.push(req.userId);
+	} else {
+		post.likes = post.likes.filter((id) => id !== String(req.userId));
+	}
+
+	const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+		new: true,
+	});
+
+	res.json(updatedPost);
 }
 
 export default router;
