250afc00bd0e2b3b638e6f7c0e2c88d0e65e1340
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-30 13:36:08 +0300

    fix dotenv

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
-dotenv.config({ path: '.env' })
+dotenv.config();
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }));
