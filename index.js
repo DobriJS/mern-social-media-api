02371304dbe4c1d9088428f8f82f9caebdf3ab4e
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-09-30 15:29:59 +0300

    test heroku depl

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 const URL = 'mongodb://DobriJS:tGwe7DhTR11xoIpA@cluster0-shard-00-00.myt24.mongodb.net:27017,cluster0-shard-00-01.myt24.mongodb.net:27017,cluster0-shard-00-02.myt24.mongodb.net:27017/mern-social?ssl=true&replicaSet=atlas-v5slnn-shard-0&authSource=admin&retryWrites=true&w=majority';
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
