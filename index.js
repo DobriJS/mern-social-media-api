50719afc32a7a371791aa1706538d85c717b50d9
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-10-01 19:01:52 +0300

    config .env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
-dotenv.config({ path: '.env'});
+dotenv.config()
 const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }));
@@ -17,8 +17,8 @@ app.get("/", (req, res) => {
 	res.send("Hello to FEED");
 });
 
-const URL = 'mongodb://DobriJS:tGwe7DhTR11xoIpA@cluster0-shard-00-00.myt24.mongodb.net:27017,cluster0-shard-00-01.myt24.mongodb.net:27017,cluster0-shard-00-02.myt24.mongodb.net:27017/mern-social?ssl=true&replicaSet=atlas-v5slnn-shard-0&authSource=admin&retryWrites=true&w=majority';
 const PORT = process.env.PORT || 4000;
+const URL = process.env.CONNECTION_URL;
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
