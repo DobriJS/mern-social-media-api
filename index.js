df24ad89ad914f1adc0f5ec170fe70042897ed2a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-10-02 23:51:19 +0300

    restore first URI

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 4000;
-const URL = process.env.DATABASE_URL;
-mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(`mongodb://DobriJS:tGwe7DhTR11xoIpA@cluster0-shard-00-00.myt24.mongodb.net:27017,cluster0-shard-00-01.myt24.mongodb.net:27017,cluster0-shard-00-02.myt24.mongodb.net:27017/mern-social?ssl=true&replicaSet=atlas-v5slnn-shard-0&authSource=admin&retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
 .catch((error) => console.log(`${error} did not connect`));
